6202c25a3df34a0f6524b4824b14057fe42915db
Poyraz INAN <ben@geik.xyz>
2021-09-12 22:21:38 +0300

    Added Application
    + Jar file added.
    + Start bash added (Require Java 16)
    + Data.xlsx File added. (Excel file)

---
+++ AutoWPMsg Application/baslat.bat
@@ -0,0 +1,6 @@
+@echo off
+echo WhatsApp otomatik mesaj modülüne hoş geldiniz.
+echo Önce excel dosyasını düzenleyip daha sonra programı tekrar başlatın.
+echo Ardından QR Kodu whatsapp ile okutun.
+java -Xmx128M -jar --enable-preview AutoWPMsg-1.0-SNAPSHOT.jar
+PAUSE
